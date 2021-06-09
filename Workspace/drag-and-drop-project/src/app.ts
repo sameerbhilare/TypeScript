3b2a565376a6ed82f3a89c8540e163fa2d5d87a8
Sameer Bhilare <bhilare.sameer@gmail.com>
2021-06-09 18:10:17 +0530

    DOM Element Selection & OOP Rendering

--- Workspace/drag-and-drop-project/src/app.ts
+++ Workspace/drag-and-drop-project/src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app') as HTMLDivElement;
+
+    // render the form
+    // this.templateElement.content gives us whatever inside of <template> tag
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+
+    this.attach();
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
